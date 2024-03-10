3f2a52bd55c5e87a5be145761ca85299bb82bed7
Skeleton Collecting Dust <skeletoncollectingdust@proton.me>
2024-03-10 18:28:15 +0000

    Added page structure and label list header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+
+import UploadIcon from './assets/upload_icon.png';
+
 import "./App.scss";
 
 const App = () => {
 	return (
-		<div>
-			Hello World!
+		<div className="label-page">
+			<div className="label-list">
+                <div className="label-header">
+                    <div>Labels</div>
+                    <div className='icon-container'>
+                        <img 
+                            className='upload-icon'
+                            src={UploadIcon}
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div className="label-preview">
+
+            </div>
 		</div>
 	);
 }
